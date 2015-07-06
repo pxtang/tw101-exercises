3c752ff6816d2ab3b6d3b4bfd399a51b2e271ce4
pxtang <petertangisme@gmail.com>
2015-07-06 11:57:15 -0700

    finished exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,14 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        if (start % 2 == 0) // if even, make it odd
+            start++;
+
+        int sum = 0;
+        for (int i = start; i < end; i += 2) { // only iterate through odds
+            sum += i;
+        }
+
+        return sum;
     }
 }
