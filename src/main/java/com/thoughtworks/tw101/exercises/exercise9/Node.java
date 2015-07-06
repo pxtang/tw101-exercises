45f2f3feca78780fdab1b06cfd6ca56fd983c59d
pxtang <petertangisme@gmail.com>
2015-07-06 13:16:24 -0700

    finished exercise 9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,44 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node left;
+    private Node right;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        Node next;
+        if (nameOfNewNode.compareToIgnoreCase(this.name) < 0) { // name after newNode
+            if (left == null) {
+                left = new Node(nameOfNewNode);
+            } else {
+                left.add(nameOfNewNode);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(nameOfNewNode);
+            } else {
+                right.add(nameOfNewNode);
+            }
+        }
+
+
     }
 
     public List<String> names() {
-        return null;
+        List<String> nameList = new ArrayList<String>();
+        if (left != null)
+            nameList.addAll(left.names());
+        nameList.add(name);
+        if (right != null)
+            nameList.addAll(right.names());
+
+        return nameList;
     }
 }
