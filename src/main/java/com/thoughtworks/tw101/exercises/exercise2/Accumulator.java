17b798bcbf792b234f467638871c887b29e6e651
pxtang <petertangisme@gmail.com>
2015-07-06 11:54:08 -0700

    Finished exercises 1, 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private static int calls = 0;
+
+    public void increment() {
+        calls ++;
+    }
+
+    public void total() {
+        System.out.println(calls);
+    }
+
 }
